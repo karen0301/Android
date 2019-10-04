bf74259d461eb9c8c8b46e8d8c702a971ff5ebb0
karen0301 <kbarchik@gmail.com>
2019-10-03 21:51:50 -0500

    This is the validate sign up 03-10-19

--- Market/app/src/main/java/com/example/market/signup.java
+++ Market/app/src/main/java/com/example/market/signup.java
@@ -3,6 +3,7 @@ package com.example.market;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -41,18 +42,65 @@ public class signup extends AppCompatActivity {
         String Passwd = "1234";
         String Lname = "McDonald";
         String Fname = "Peter";
-        //4. Crear el contenedor de valores
-        ContentValues DATA = new ContentValues(); //crea el contenedor
-        // 5. Crear el paquete de datos, create data pack
-        DATA.put("email", Email);
-        DATA.put("password", Passwd);
-        DATA.put("firstname", Fname);
-        DATA.put("lastname", Lname);
-        //6. Save data into market db, guardar los datos
-        market.insert("users", null, DATA);
-        Toast.makeText(this, "The user has been created!", Toast.LENGTH_LONG).show();
-        //7. Close connection
-        market.close();
 
+        //**** Validate: Do not repeat Email
+        //Call validateEmail function/method
+        // los valores que llegan al status--- status sera un true
+
+        //**** ==== methodo boolean
+        //boolean status = validateEmail();
+    //podemos comentar el metodo boolean status = validateEmail(), y hacer la validacion de otra forma
+
+        //4. Crear el contenedor de valores y validar que los campos no lleguen vacios-
+        //Validate empty data and create content values (|| alt+1)
+
+        if(!Email.isEmpty() && !Passwd.isEmpty() && !Lname.isEmpty() && !Fname.isEmpty()){
+
+            //if (!status){
+
+            Cursor row = market.rawQuery("SELECT email " +
+                    "FROM users WHERE email = "+Email , null);
+            if (!row.moveToFirst()){
+
+                ContentValues DATA = new ContentValues(); //crea el contenedor
+
+                // 5. Crear el paquete de datos, create data pack
+                DATA.put("email", Email);
+                DATA.put("password", Passwd);
+                DATA.put("firstname", Fname);
+                DATA.put("lastname", Lname);
+                //6. Save data into market db, guardar los datos
+                market.insert("users", null, DATA);
+                Toast.makeText(this, "The user has been created!", Toast.LENGTH_LONG).show();
+                //7. Close connection
+                market.close();
+            }else{
+                Toast.makeText(this, "The user already exists.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "You must complete all fields.", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    //====
+    //esta validacion servira para hacer el login tambien
+    //es una clase
+    //metodo validatEmail
+    //getWritableDatabase permite lectura y escritura
+    //View view es abstracto
+
+   /* public boolean validateEmail(View view) {
+        connectionDB manager = new connectionDB(this, "data", null, 1);
+        SQLiteDatabase market = manager.getWritableDatabase();
+        String Email = EMAIL.getText().toString();
+        //rawQuery recorre la tabla, En row se guarda la consulta
+        Cursor row = market.rawQuery("SELECT email " +
+                "FROM users WHERE email = "+Email , null);
+        if(row.moveToFirst()){
+            return true;
+        }else{
+            return false; //estos valores se recuperan en Callvalidate email antes del punto 4.
+        }
+
+    }*/
 }
